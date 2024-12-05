283b442629182a97203857a5fe23a9daee98b9c3
fershad <27988517+fershad@users.noreply.github.com>
2024-12-05 22:10:00 +0800

    add an initial greenlinks check

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { parseHTML } from "linkedom";
+import { hosting } from "@tgwf/co2"
+import markdownit from 'markdown-it'
 
 export default function(eleventyConfig) {
 
@@ -34,6 +37,55 @@ export default function(eleventyConfig) {
 	eleventyConfig.addFilter("limitWords", function(value, limit = 50) {
 		// Return the first `limit` words, plus an ellipsis if needed
 		return value.split(" ").slice(0, limit).join(" ") + (value.split(" ").length > limit ? "..." : "");
+	});
+
+	eleventyConfig.addShortcode("greenLinks", async function() {
+		const content = this.page.rawInput;
+		const filetype = this.page.inputPath.split('.').pop();
+
+		let parsedContent;
+
+		if (filetype === 'md') {
+			const md = markdownit();
+			const mdContent = md.render(content);
+			parsedContent = parseHTML(mdContent);
+		} else {
+			parsedContent = parseHTML(content);
+		}
+
+		const { document } = parsedContent;
+		const links = [...document.querySelectorAll('a')];
+		const allValidLinks = links.filter((link) => /^(https?\:\/\/|\/\/)/i.test(link.href));
+
+		let greenLinksCount = 0;
+
+		if (links.length > 0) {
+			//   Get all unique hostnames from the links
+			const uniqueHostnames = [...new Set(allValidLinks.map((link) => new URL(link.href).hostname))];
+
+		// 	//   Remove the hostnames that should be ignored
+		// 	ignore.forEach((hostname) => {
+		// 		const ignoreHostnameIndex = uniqueHostnames.indexOf(hostname);
+		// 	if (ignoreHostnameIndex > -1) {
+		// 		uniqueHostnames.splice(ignoreHostnameIndex, 1);
+		// 	}
+		//   });
+	  
+			  //   Check if the hostnames are green
+			  const greenHostnames = await hosting(uniqueHostnames);
+	  
+			  //   Add the green attribute to the links
+			  allValidLinks.forEach((link) => {
+				  const hostname = new URL(link.href).hostname;
+				  if (greenHostnames.includes(hostname)) {
+					  greenLinksCount++;
+				  }
+			  });
+		}
+
+		return `<span class="green-links">${greenLinksCount} green links</span> / ${links.length} total links`;
+
+		
 	});
     
     return {
