c3957e6379859ac07c9064e30a812f87420969a3
fershad <27988517+fershad@users.noreply.github.com>
2024-12-05 09:14:54 +0800

    fix data

--- src/_data/readwise.js
+++ src/_data/readwise.js
@@ -1,9 +1,13 @@
 import Fetch from "@11ty/eleventy-fetch";
 const token = process.env.READWISE_TOKEN;
-const updated = new Date().toUTCString();
+const currentDate = new Date()
+const updated = currentDate.toUTCString();
+
+currentDate.setDate(currentDate.getDate() - 30);
+const isoDate = currentDate.toISOString();
 
 export default async function () {
-	let url = "https://readwise.io/api/v3/list/?location=archive";
+	let url = `https://readwise.io/api/v3/list/?location=archive&updatedAfter=${isoDate}`;
 
 	let json = await Fetch(url, {
 		duration: "1d", // 1 day caching
@@ -14,11 +18,15 @@ export default async function () {
         }
     }});
 
+    // NOTE: The original JSON does include other things like notes & highlights. Maybe some that can be used later?
+
+    const list = json.results.filter(result => !result.parent_id).slice(0, 30);
     // Limit to 5 items
     json = json.results.filter(result => !result.parent_id).slice(0, 5);
 
 	return {
         updated,
-        reader: json
+        reader: json,
+        list
     };
 };
