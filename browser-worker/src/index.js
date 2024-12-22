44b71edb5faa86d54c6fca951247b316d0021b4d
fershad <27988517+fershad@users.noreply.github.com>
2024-12-23 01:03:32 +0800

    og-image generator

---
+++ browser-worker/src/index.js
@@ -0,0 +1,43 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import puppeteer from "@cloudflare/puppeteer";
+
+export default {
+  async fetch(request, env) {
+    const { searchParams } = new URL(request.url);
+    let title = searchParams.get("title");
+	const url = "https://beta.fershad.com/og/?title=" + title;
+    let img;
+    if (title) {
+      img = await env.FERSHAD_OG_IMAGES.get(title, { type: "arrayBuffer" });
+      if (img === null) {
+        const browser = await puppeteer.launch(env.MYBROWSER);
+        const page = await browser.newPage();
+		page.setViewport({
+			width: 1200,
+			height: 630,
+			deviceScaleFactor: 2,
+		});
+        await page.goto(url);
+        img = await page.screenshot();
+        await env.FERSHAD_OG_IMAGES.put(title, img);
+        await browser.close();
+      }
+      return new Response(img, {
+        headers: {
+          "content-type": "image/jpeg",
+        },
+      });
+    } else {
+      return new Response("Please add an ?title= parameter");
+    }
+  },
+};
