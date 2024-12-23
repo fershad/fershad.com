ccf61d3fe9b571f818c789f9b8b31dcb5737dd18
fershad <27988517+fershad@users.noreply.github.com>
2024-12-23 23:43:39 +0800

    adjust codepen iframe

--- eleventy.config.js
+++ eleventy.config.js
@@ -285,7 +285,7 @@ export default function(eleventyConfig) {
 	});
 
 	eleventyConfig.addShortcode("codepenIframe", function(penId, title, user = "fishintaiwan") {
-		return `<figure><iframe data-gaw-remove height="300" style="width: 100%;" scrolling="no" title="${title}" src="https://codepen.io/fishintaiwan/embed/preview/${penId}?default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+		return `<figure><iframe data-gaw-remove height="300" style="width: 100%;" scrolling="no" title="${title}" src="https://codepen.io/fishintaiwan/embed/${penId}?default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
   See the Pen <a href="https://codepen.io/${user}/pen/${penId}">
 </iframe>
 <figcaption><p><a href="https://codepen.io/${user}/pen/${penId}">View ${title} by @${user} on CodePen</a></p></figcaption></figure>`;
