7a1b5b1cf7645a22e22c69c2670d85110c653752
fershad <27988517+fershad@users.noreply.github.com>
2024-12-23 17:33:42 +0800

    fix dev

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,4 @@
-export const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.ELEVENTY_RUN_MODE === "serve" ? true : false;
 export const baseURL = dev ? 'http://localhost:8080' : 'https://fershad.com';
 export const title = 'Fershad Irani';
 
