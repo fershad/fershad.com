0af18f478b6c5763c5a435199820b619cba7f3e2
fershad <27988517+fershad@users.noreply.github.com>
2024-12-29 20:38:41 +0800

    try catch

--- workers/grid-aware/src/index.js
+++ workers/grid-aware/src/index.js
@@ -49,130 +49,136 @@ export default {
 		const response = await fetch(request.url, {
 			method: "GET",
 		});
-		// Then check if the request content type is HTML. If not, return the request and headers as is.
-		const contentType = response.headers.get('content-type');
 
-		// Check if the content type is HTML.
-		// If not, return the response as is.
-		if (!contentType || !contentType.includes('text/html')) {
-			return new Response(response.body, {
-				...response,
-			});
-		}
+		try {
+			// Then check if the request content type is HTML. If not, return the request and headers as is.
+			const contentType = response.headers.get('content-type');
 
-		const COOKIE_NAME = "gaw-status";
-		const cookie = request.headers.get("cookie");
+			// Check if the content type is HTML.
+			// If not, return the response as is.
+			if (!contentType || !contentType.includes('text/html')) {
+				return new Response(response.body, {
+					...response,
+				});
+			}
 
-
-		  if ((cookie && cookie.includes(`${COOKIE_NAME}=disable`)) || request.url.includes("/og/?") || request.url.includes("/api/") || request.url.includes("/img/")) {
-			// if user has rejected the grid-aware site
-			return new Response(response.body, {
-				...response,
-				headers: {
-					...response.headers,
-					'grid-aware': 'opt-out',
-				},
-			});
-		  }
+			const COOKIE_NAME = "gaw-status";
+			const cookie = request.headers.get("cookie");
 
 
-		// Get the country of the user from the Cloudflare data
-		const cfData = await getLocation(request, {
-			"mode": "country"
-		})
+			  if ((cookie && cookie.includes(`${COOKIE_NAME}=disable`)) || request.url.includes("/og/?") || request.url.includes("/api/") || request.url.includes("/img/")) {
+				// if user has rejected the grid-aware site
+				return new Response(response.body, {
+					...response,
+					headers: {
+						...response.headers,
+						'grid-aware': 'opt-out',
+					},
+				});
+			  }
 
-		let { country } = cfData;
 
-		// If the country is not found, return the response as is.
-		if (!country) {
-			return new Response(response.body, response);
-		}
+			// Get the country of the user from the Cloudflare data
+			const cfData = await getLocation(request, {
+				"mode": "country"
+			})
 
-		let gridData = await fetchDataFromKv(env, country).then((data) => { return JSON.parse(data) });
+			let { country } = cfData;
 
-		if (gridData?.status === 'error') {
-			const resp = setResponseHeaders(response, gridData);
-			return resp;
-		}
+			// If the country is not found, return the response as is.
+			if (!country) {
+				return new Response(response.body, response);
+			}
 
-		if (!gridData) {
-			console.log('Fetching grid data for the country');
-			// Fetch the grid data for the country using the @greenweb/grid-aware-websites package
-			gridData = await gridAwarePower(country, env.EMAPS_API_KEY, {
-				mode: "low-carbon",
-			});
+			let gridData = await fetchDataFromKv(env, country).then((data) => { return JSON.parse(data) });
 
-			// If the grid data is not found, return the response as is.
-			if (gridData.status === 'error') {
-				const resp = setResponseHeaders(response, gridData)
+			if (gridData?.status === 'error') {
+				const resp = setResponseHeaders(response, gridData);
 				return resp;
 			}
 
-			await saveDataToKv(env, country, JSON.stringify(gridData));
-		} else {
-			console.log('Using cached grid data');
-		}
+			if (!gridData) {
+				console.log('Fetching grid data for the country');
+				// Fetch the grid data for the country using the @greenweb/grid-aware-websites package
+				gridData = await gridAwarePower(country, env.EMAPS_API_KEY, {
+					mode: "low-carbon",
+				});
 
-		// If the gridAware value is set to true, then let's modify the page
-		if (gridData.gridAware) {
-			// Check if the response is already stored in KV
-			const cachedResponse = await fetchPageFromKv(env, request.url);
-			// const cachedResponse = await env.GAW_CACHE.get(request.url);
+				// If the grid data is not found, return the response as is.
+				if (gridData.status === 'error') {
+					const resp = setResponseHeaders(response, gridData)
+					return resp;
+				}
 
-			if (cachedResponse) {
-				console.log('Returning cached response');
-				const resp = setResponseHeaders(cachedResponse, gridData)
-				resp.headers.append('GAW-Cached', 'true');
+				await saveDataToKv(env, country, JSON.stringify(gridData));
+			} else {
+				console.log('Using cached grid data');
+			}
+
+			// If the gridAware value is set to true, then let's modify the page
+			if (gridData.gridAware) {
+				// Check if the response is already stored in KV
+				const cachedResponse = await fetchPageFromKv(env, request.url);
+				// const cachedResponse = await env.GAW_CACHE.get(request.url);
+
+				if (cachedResponse) {
+					console.log('Returning cached response');
+					const resp = setResponseHeaders(cachedResponse, gridData)
+					resp.headers.append('GAW-Cached', 'true');
+					resp.headers.append('Content-Type', 'text/html;charset=UTF-8');
+					return resp;
+				}
+
+				console.log('Modifying the page');
+
+				/*
+					Here you can use the HTMLRewriter API, or you can
+					use other methods such as redirecting the user to a different page,
+					or using a regular expression to change the CSS file used by the page.
+
+					You can also import other libraries like Cheerio or JSDOM to modify the page
+					if you are more comfortable with those.
+				*/
+
+				// Remove the elements with the data-gaw-remove attribute & add a class to deglitch the page.
+				const modifyHTML = new HTMLRewriter().on('[data-gaw-remove]', {
+					element(element) {
+						element.remove();
+					},
+				}).on('body', {
+					element(element) {
+						element.setAttribute('class', 'deglitch');
+					},
+				});
+
+				let modifiedResponse = new Response(modifyHTML.transform(response).body, {
+					...response,
+					headers: {
+						...response.headers,
+						'Content-Type': 'text/html;charset=UTF-8',
+						// We can also add some of the grid-aware data to the headers of the response
+						'GAW-cached': 'false',
+					},
+				});
+
+
+				// Store the modified response in the KV for 24 hours
+				// await env.GAW_CACHE.put(request.url, modifiedResponse.clone().body, { expirationTtl: 60 * 60 * 24 });
+				await savePageToKv(env, request.url, modifiedResponse.clone());
+				const resp = setResponseHeaders(modifiedResponse, gridData);
 				resp.headers.append('Content-Type', 'text/html;charset=UTF-8');
+				resp.headers.append('GAW-Cached', 'false');
 				return resp;
 			}
 
-			console.log('Modifying the page');
-
-			/*
-				Here you can use the HTMLRewriter API, or you can
-				use other methods such as redirecting the user to a different page,
-				or using a regular expression to change the CSS file used by the page.
-
-				You can also import other libraries like Cheerio or JSDOM to modify the page
-				if you are more comfortable with those.
-			*/
-
-			// Remove the elements with the data-gaw-remove attribute & add a class to deglitch the page.
-			const modifyHTML = new HTMLRewriter().on('[data-gaw-remove]', {
-				element(element) {
-					element.remove();
-				},
-			}).on('body', {
-				element(element) {
-					element.setAttribute('class', 'deglitch');
-				},
-			});
-
-			let modifiedResponse = new Response(modifyHTML.transform(response).body, {
-				...response,
-				headers: {
-					...response.headers,
-					'Content-Type': 'text/html;charset=UTF-8',
-					// We can also add some of the grid-aware data to the headers of the response
-					'GAW-cached': 'false',
-				},
-			});
-
-
-			// Store the modified response in the KV for 24 hours
-			// await env.GAW_CACHE.put(request.url, modifiedResponse.clone().body, { expirationTtl: 60 * 60 * 24 });
-			await savePageToKv(env, request.url, modifiedResponse.clone());
-			const resp = setResponseHeaders(modifiedResponse, gridData);
+			// If the gridAware value is set to false, then return the response as is.
+			const resp = setResponseHeaders(response, gridData);
 			resp.headers.append('Content-Type', 'text/html;charset=UTF-8');
 			resp.headers.append('GAW-Cached', 'false');
 			return resp;
+		} catch (error) {
+			console.error(error);
+			return new Response(response.body, response);
 		}
-
-		// If the gridAware value is set to false, then return the response as is.
-		const resp = setResponseHeaders(response, gridData);
-		resp.headers.append('Content-Type', 'text/html;charset=UTF-8');
-		resp.headers.append('GAW-Cached', 'false');
-		return resp;
 	},
 };
