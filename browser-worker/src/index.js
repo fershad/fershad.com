07843f702c0356679d23008b978491c99066d777
fershad <27988517+fershad@users.noreply.github.com>
2025-01-06 15:29:08 +0800

    update browser worker

--- browser-worker/src/index.js
+++ browser-worker/src/index.js
@@ -14,12 +14,10 @@ export default {
   async fetch(request, env) {
     const { searchParams } = new URL(request.url);
     let title = searchParams.get("title");
-		let page = searchParams.get("page");
 
 		const url = "https://fershad.com/og/?title=" + title;
-    let img;
-    if (title) {
-      img = await env.FERSHAD_OG_IMAGES.get(`${page}_${title}`, { type: "arrayBuffer" });
+    let img = await env.FERSHAD_OG_IMAGES.get(`${title}`, { type: "arrayBuffer" });
+
       if (img === null) {
         const browser = await puppeteer.launch(env.MYBROWSER);
         const page = await browser.newPage();
@@ -30,19 +28,17 @@ export default {
 		});
         await page.goto(url);
         img = await page.screenshot();
-        await env.FERSHAD_OG_IMAGES.put(`${page}_${title}`, img, {
+        await env.FERSHAD_OG_IMAGES.put(`${title}`, img, {
           // cache for 1 year
 					expirationTtl: 60 * 60 * 24 * 365,
         });
         await browser.close();
       }
+
       return new Response(img, {
         headers: {
           "content-type": "image/jpeg",
         },
-      });
-    } else {
-      return new Response("Please add an ?title= parameter");
-    }
+      })
   },
 };
