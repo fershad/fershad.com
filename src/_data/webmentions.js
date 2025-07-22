b884f1515c5460d986a6efac474b7455d3e81b67
fershad <27988517+fershad@users.noreply.github.com>
2025-07-23 06:39:21 +0800

    Add webmentions _data and _partial

---
+++ src/_data/webmentions.js
@@ -0,0 +1,15 @@
+// Fetch webmentions from webmention.io API
+import EleventyFetch from "@11ty/eleventy-fetch";
+
+export default async function () {
+  const WEBMENTIONS_BOBM = process.env.WEBMENTION_IO_TOKEN;
+  const url = `https://webmention.io/api/mentions.jf2?token=${WEBMENTIONS_BOBM}&per-page=1000`;
+  const res = EleventyFetch(url, {
+    duration: "1h",
+    type: "json",
+  });
+  const webmentions = await res;
+  return {
+    mentions: webmentions.children,
+  };
+}
