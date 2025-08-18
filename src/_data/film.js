3d1ea16969cc01f0669ae83f643ab7ef39f62a26
fershad <27988517+fershad@users.noreply.github.com>
2025-08-18 17:01:55 +0800

    remove eleventy image on film

--- src/_data/film.js
+++ src/_data/film.js
@@ -3,9 +3,10 @@ import Fetch from "@11ty/eleventy-fetch";
 export default async function () {
   let url = "https://film.fershad.com/all.json?v=2.0";
 
-  let json = await fetch("https://film.fershad.com/all.json").then((res) =>
-    res.json(),
-  );
+  let json = await Fetch(url, {
+    duration: "90d", // 3 months caching
+    type: "json", // we’ll parse JSON for you
+  });
 
   // Filter the images that don't have descriptions
   json.images = json.images.filter(
