a57f4c4a009bf068f6e85cc9f13caaf4882ba8da
fershad <27988517+fershad@users.noreply.github.com>
2025-08-18 16:50:10 +0800

    update film endpoint to bust cache

--- src/_data/film.js
+++ src/_data/film.js
@@ -1,16 +1,18 @@
 import Fetch from "@11ty/eleventy-fetch";
 
 export default async function () {
-	let url = "https://film.fershad.com/all.json";
+  let url = "https://film.fershad.com/all.json?v=2.0";
 
-	let json = await Fetch(url, {
-		duration: "90d", // 3 months caching
-		type: "json", // we’ll parse JSON for you
-	});
+  let json = await Fetch(url, {
+    duration: "90d", // 3 months caching
+    type: "json", // we’ll parse JSON for you
+  });
 
-    // Filter the images that don't have descriptions
-    json.images = json.images.filter(image => image.description && image.portrait === "true");
-    const random = json.images[Math.floor(Math.random() * json.images.length)];
+  // Filter the images that don't have descriptions
+  json.images = json.images.filter(
+    (image) => image.description && image.portrait === "true",
+  );
+  const random = json.images[Math.floor(Math.random() * json.images.length)];
 
-	return random;
-};
+  return random;
+}
