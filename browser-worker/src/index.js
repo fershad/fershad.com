05398929d14ff1488742be649233f6982f02aac5
fershad <27988517+fershad@users.noreply.github.com>
2025-09-01 22:39:47 +0800

    adjust viewport for image

--- browser-worker/src/index.js
+++ browser-worker/src/index.js
@@ -8,37 +8,37 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import puppeteer from "@cloudflare/puppeteer";
+import puppeteer from '@cloudflare/puppeteer';
 
 export default {
-  async fetch(request, env) {
-    const { searchParams } = new URL(request.url);
-    let title = searchParams.get("title");
+	async fetch(request, env) {
+		const { searchParams } = new URL(request.url);
+		let title = searchParams.get('title');
 
-		const url = "https://fershad.com/og/?title=" + title;
-    let img = await env.FERSHAD_OG_IMAGES.get(`${title}`, { type: "arrayBuffer" });
+		const url = 'https://fershad.com/og/?title=' + title;
+		let img = await env.FERSHAD_OG_IMAGES.get(`${title}`, { type: 'arrayBuffer' });
 
-      if (img === null) {
-        const browser = await puppeteer.launch(env.MYBROWSER);
-        const page = await browser.newPage();
-		page.setViewport({
-			width: 1200,
-			height: 630,
-			deviceScaleFactor: 2,
+		if (img === null) {
+			const browser = await puppeteer.launch(env.MYBROWSER);
+			const page = await browser.newPage();
+			page.setViewport({
+				width: 1200,
+				height: 627,
+				deviceScaleFactor: 2,
+			});
+			await page.goto(url);
+			img = await page.screenshot();
+			await env.FERSHAD_OG_IMAGES.put(`${title}`, img, {
+				// cache for 1 year
+				expirationTtl: 60 * 60 * 24 * 365,
+			});
+			await browser.close();
+		}
+
+		return new Response(img, {
+			headers: {
+				'content-type': 'image/jpeg',
+			},
 		});
-        await page.goto(url);
-        img = await page.screenshot();
-        await env.FERSHAD_OG_IMAGES.put(`${title}`, img, {
-          // cache for 1 year
-					expirationTtl: 60 * 60 * 24 * 365,
-        });
-        await browser.close();
-      }
-
-      return new Response(img, {
-        headers: {
-          "content-type": "image/jpeg",
-        },
-      })
-  },
+	},
 };
