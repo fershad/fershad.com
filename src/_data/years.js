15dc4f1f42ab73f03d9f7e7abe12558411932e7f
fershad <27988517+fershad@users.noreply.github.com>
2025-12-11 14:08:08 +0800

    yearly pages

--- src/_data/years.js
+++ src/_data/years.js
@@ -1,10 +1,8 @@
 export const currentYear = new Date().getFullYear();
 const startYear = 2019;
 
-const blogYears = [];
+export const blogYears = [];
 
 for (let i = startYear; i <= currentYear; i++) {
   blogYears.push(i);
 }
-
-export default blogYears;
