3db85ec7334fc987e2c305857320b82c6d08243d
fershad <27988517+fershad@users.noreply.github.com>
2026-08-11 23:44:28 +0800

    add services and workshops

---
+++ src/_data/services.js
@@ -0,0 +1,31 @@
+import { baseURL } from "./site.js";
+
+export default [
+  {
+    slug: "training",
+    title: "Training",
+    shortContent: "Tailored training sessions for leadership and teams.",
+    content:
+      "Training for leadership, sustainability, and technology teams. I can deliver tailored training sessions for topics ranging from an introduction to digital sustainability, to explaining the Web Sustainability Guidelines, to exploring the sustainability reporting landscape.",
+    ctaText: "Organise a training session",
+    emailSubject: "Training Inquiry",
+  },
+  {
+    slug: "advisory",
+    title: "Advisory",
+    shortContent: "Trusted digital sustainability guidance.",
+    content:
+      "Trusted guidance to help you understand your digital sustainability profile, identify gaps, and develop actionable plans to get your organisation to its sustainability goals. Useful for organisations that are just getting started, working on sustainability reports, or who want to responsibly select the digital suppliers they use.",
+    ctaText: "Arrange an assessment",
+    emailSubject: "Advisory Inquiry",
+  },
+  {
+    slug: "support",
+    title: "Support",
+    shortContent: "Augment your sustainability team with technical expertise.",
+    content:
+      "Augment your sustainability team with technical expertise. Ideal for organisations of all sizes who need ongoing support for their sustainability teams to achieve their objectives. Whether you need someone your trust to embed within your team, or just be around to answer questions, we can scope an appropriate engagement for your organisation's needs.",
+    ctaText: "Reach out for a quote",
+    emailSubject: "Sustainability Support Inquiry",
+  },
+];
---
+++ src/_data/workshops.js
@@ -0,0 +1,22 @@
+import { baseURL } from "./site.js";
+
+export default [
+  {
+    slug: "estimating-digital-carbon-emissions-workshop",
+    title: "Introduction: Estimating digital carbon emissions workshop",
+    greenweb: true,
+    content:
+      "A two hour workshop that introduces participants to the digital carbon emissions, how they are generated, why organisations may need to report them, and how to go about estimating them.",
+    ctaText: "Arrange a workshop",
+    emailSubject: "Intro Workshop Inquiry",
+  },
+  {
+    slug: "estimating-digital-carbon-emissions-workshop",
+    title: "Advanced: Measuring digital carbon emissions workshop",
+    greenweb: true,
+    content:
+      "A two hour hands on workshop that guides participants through multiple tools they can use to more accurately measure digital carbon emissions.",
+    ctaText: "Arrange a session",
+    emailSubject: "Advanced Workshop Inquiry",
+  },
+];
